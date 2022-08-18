45c0627c5ec28f284cc625514e668d03d6bab4e3
daanibhaya <daanishbhayani1209@gmail.com>
2022-08-18 04:03:20 -0400

    finalize layout, added decimal logic

--- script.js
+++ script.js
@@ -1,39 +1,120 @@
+let firstOperand = ''
+let secondOperand = ''
+let currentOperation = null
+let shouldResetScreen = false
 
-console.log(add(12, 15))
-console.log(subtract(12, 15))
-console.log(multiply(4, 5))
-console.log(divide(22, 3))
-console.log(operate(12, "subtract", 10))
+const keyboardInput = document.querySelector('data-number')
+const numberButtons = document.querySelectorAll('[data-number]')
+const operatorButtons = document.querySelectorAll('[data-operator]')
+const equalsButton = document.getElementById('equalsBtn')
+const clearButton = document.getElementById('clearBtn')
+const deleteButton = document.getElementById('deleteBtn')
+const pointButton = document.getElementById('pointBtn')
+const lastOperationScreen = document.getElementById('lastOperationScreen')
+const currentOperationScreen = document.getElementById('currentOperationScreen')
 
-function add(numOne, numTwo) {
-    let addNum = numOne + numTwo;
-    return addNum
-}
-function subtract(numOne, numTwo) {
-    let subNum = numOne - numTwo;
-    return subNum
-}add
-function multiply(numOne, numTwo) {
-    let multiNum = numOne * numTwo;
-    return multiNum
-}
-function divide(numOne, numTwo) {
-    let diviNum = numOne / numTwo;
-    return diviNum
-}
-function operate(numOne, action, numTwo) {
-    if (action === "add") {
-        return add(numOne, numTwo)
-    }
-    if (action == "subtract") {
-        return subtract(numOne, numTwo)
-    }
-    if (action === "multiply") {
-        return multiply(numOne, numTwo)
-    }
-    if (action == "divide") {
-        return divide(numOne, numTwo)
-    }
+equalsButton.addEventListener('click', evaluate)
+clearButton.addEventListener('click', clear)
+deleteButton.addEventListener('click', deleteNumber)
+pointButton.addEventListener('click', appendPoint)
+
+numberButtons.forEach((button) =>
+  button.addEventListener('click', () => appendNumber(button.textContent))
+)
+
+operatorButtons.forEach((button) =>
+  button.addEventListener('click', () => setOperation(button.textContent))
+)
+
+function appendNumber(number) {
+  if (currentOperationScreen.textContent === '0' || shouldResetScreen)
+    resetScreen()
+  currentOperationScreen.textContent += number
 }
 
+function resetScreen() {
+  currentOperationScreen.textContent = ''
+  shouldResetScreen = false
+}
 
+function clear() {
+  currentOperationScreen.textContent = '0'
+  lastOperationScreen.textContent = ''
+  firstOperand = ''
+  secondOperand = ''
+  currentOperation = null
+}
+
+function appendPoint() {
+  if (shouldResetScreen) resetScreen()
+  if (currentOperationScreen.textContent === '')
+    currentOperationScreen.textContent = '0'
+  if (currentOperationScreen.textContent.includes('.')) return
+  currentOperationScreen.textContent += '.'
+}
+
+function deleteNumber() {
+  currentOperationScreen.textContent = currentOperationScreen.textContent
+    .toString()
+    .slice(0, -1)
+}
+
+function setOperation(operator) {
+  if (currentOperation !== null) evaluate()
+  firstOperand = currentOperationScreen.textContent
+  currentOperation = operator
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperation}`
+  shouldResetScreen = true
+}
+
+function evaluate() {
+  if (currentOperation === null || shouldResetScreen) return
+  if (currentOperation === '÷' && currentOperationScreen.textContent === '0') {
+    alert("Theoretical physical singularities include black hole (density) centroids, but their physical manifestations are vague at best. They are essentially shielded from the universe by their event horizons which are better known. It is also axiomatized by many physicists that physical law singularities point to physical impossibilities or physical law errors (hence the error in divide-by-zero errors) that in turn point to the need for refined theories, that is, physical singularities (or divide-by-zero phenomena) do not exist. Before a physical singularity is reached, other things break down long before that.")
+    return
+  }
+  secondOperand = currentOperationScreen.textContent
+  currentOperationScreen.textContent = roundResult(
+    operate(currentOperation, firstOperand, secondOperand)
+  )
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`
+  currentOperation = null
+}
+
+function roundResult(number) {
+  return Math.round(number * 1000) / 1000
+}
+
+function add(a, b) {
+  return a + b
+}
+
+function substract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function operate(operator, a, b) {
+  a = Number(a)
+  b = Number(b)
+  switch (operator) {
+    case '+':
+      return add(a, b)
+    case '−':
+      return substract(a, b)
+    case '×':
+      return multiply(a, b)
+    case '÷':
+      if (b === 0) return null
+      else return divide(a, b)
+    default:
+      return null
+  }
+}
